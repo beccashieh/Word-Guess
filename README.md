8cf3ac7ae482d3c23524c9d17249784b15017bf4
BShieh <rebeccasshieh@gmail.com>
2019-05-07 15:17:04 -0400

    added colors to displayed messages, updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Word-Guess
-Command line word guess game.
+##Command line word guess game.
+- This JavaScript Node application uses constructors and linked files to take a user input (guess) and compare it to a randomly selected word. 
+- The letter.js file contains the letter constructor which checks the letter is entered and either displays the correctly guessed letter or displays the placeholder underscore.
+- The word.js file contains the word constructor which is dependent on the letter.js file to grab the letters, create the word, and display it.
+- The game logic is contained in the index.js file. This file is dependent on the word.js file, as well as the colors npm module, and inquirer. 
+
+
+## Instructions
+- From the terminal, user will need to type:
+```
+node index.js
+
+```
+- The user will be prompted to guess a letter for the randomly chosen word. 
+- The user will have 8 chances to guess the correct letters for the word. 
+- Correct guesses will be displayed.
+- If the user runs out of guesses, a message will appear letting them know the correct word and the game will start again. 
+- If the user guesses correctly, a message will appear letting them know they guessed the word. 
+
+## Technologies used
+
+- JavaScript
+- node.js
+- VS Code
+
--- index.js
+++ index.js
@@ -1,89 +1,101 @@
 //Modules
 const inquirer = require("inquirer");
 const Word = require("./word");
-const colors = require('colors');
+const colors = require("colors");
 
-wordList = ["APPLE", "MANGO", "PINEAPPLE", "PERSIMMON", "CANTELOUPE", "PAPAYA", "BANANA"];
+wordList = [
+  "APPLE",
+  "MANGO",
+  "PINEAPPLE",
+  "PERSIMMON",
+  "CANTELOUPE",
+  "PAPAYA",
+  "BANANA"
+];
 let select = 0;
 let chosenWord = "";
 let gameWord = "";
 let counter = 0;
 
 //Chooses a word from the wordList array
-function startGame(){
-    select = Math.floor(Math.random() * wordList.length);
-    chosenWord = wordList[select];
-    gameWord = new Word(chosenWord);
-    gameWord.createWord();
-    if (select > -1){
-        wordList.splice(select, 1);
-    }
-    console.log("You get 8 letter guesses to correctly find the type of fruit.\n Good luck!");
-    promptUser();
+function startGame() {
+  select = Math.floor(Math.random() * wordList.length);
+  chosenWord = wordList[select];
+  gameWord = new Word(chosenWord);
+  gameWord.createWord();
+  if (select > -1) {
+    wordList.splice(select, 1);
+  }
+  console.log(
+    "You get 8 letter guesses to correctly find the type of fruit.\n Good luck!"
+      .cyan
+  );
+  promptUser();
 }
 
 //Allows the user to input guesses and restart the game if they run out of guesses.
-function promptUser(){
-    if (counter<8){
-        console.log(gameWord.displayWord());
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "letter",
-                message: "\n Pick a letter and press enter."
-            }
-        ]).then(function(data){
-            checkAnswer(data);
-        });
-    }
-    else {
-        console.log("\n Sorry, you ran out of guesses.\n");
-        console.log(`The correct answer was ${chosenWord}`);
-        chosenWord = "";
-        gameWord = "";
-        select = 0;
-        counter = 0;
-        startGame();
-    }
+function promptUser() {
+  if (counter < 8) {
+    console.log(gameWord.displayWord());
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "letter",
+          message: "\n Pick a letter and press enter."
+        }
+      ])
+      .then(function(data) {
+        checkAnswer(data);
+      });
+  } else {
+    console.log("\n Sorry, you ran out of guesses.\n".red);
+    console.log(`The correct answer was ${chosenWord}`.bgGreen);
+    chosenWord = "";
+    gameWord = "";
+    select = 0;
+    counter = 0;
+    startGame();
+  }
 }
 
 //Checks to make sure user input was a letter and compares it against the possible letters in the selected word
-function checkAnswer(data){
-    if ((data.letter.length===1) && /^[a-zA-Z]+/.test(data.letter)){
-        const letterChecker = data.letter.toUpperCase();
-        const temp = gameWord.displayWord();
-        gameWord.checkGuess(letterChecker);
-        if (temp === gameWord.displayWord()){
-            console.log("\n Sorry, wrong letter!");
-            counter++;
-            console.log(`(8 - ${counter}) guesses remaining.`);
-            promptUser();
-        }
-        else {
-            correctGuess();
-        }
+function checkAnswer(data) {
+  if (data.letter.length === 1 && /^[a-zA-Z]+/.test(data.letter)) {
+    const letterChecker = data.letter.toUpperCase();
+    const temp = gameWord.displayWord();
+    gameWord.checkGuess(letterChecker);
+    if (temp === gameWord.displayWord()) {
+      console.log("\n Sorry, wrong letter!".bgYellow);
+      counter++;
+      console.log(8 - counter + " guesses remaining.".cyan);
+      promptUser();
+    } else {
+      correctGuess();
     }
-        else {
-            console.log(`\nPlease enter a letter, one at a time.\n`);
-        }
-    } 
+  } else {
+    console.log(`\nPlease enter a letter, one at a time.\n`.bgRed);
+    promptUser();
+  }
+}
 
 //If the user guesses the correct letter, the word array displays the word with the guessed letters.
 //If the entire word is correct, meaning letters are filled in, the game will restart.
 function correctGuess() {
-    console.log(`\n You guessed right!\n`);
-    if (chosenWord.replace(/ /g,"") == (gameWord.displayWord()).replace(/ /g,"")){
-        console.log(gameWord.displayWord());
-        console.log(`\n You Win!!!!\n`);
-        chosenWord = "";
-        gameWord = "";
-        select = 0;
-        counter = 0;
-        startGame();
-    }
-    else {
-        promptUser();
-    }
+  console.log(`\n You guessed right!\n`.bgBlue);
+  if (
+    chosenWord.replace(/ /g, "") == gameWord.displayWord().replace(/ /g, "")
+  ) {
+    console.log(gameWord.displayWord());
+    console.log(`\n You Win!!!!\n`.bgMagenta);
+    chosenWord = "";
+    gameWord = "";
+    select = 0;
+    counter = 0;
+    startGame();
+  } else {
+    promptUser();
+  }
 }
 
 startGame();
